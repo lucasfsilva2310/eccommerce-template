c3ddc411057726b1a1e117aed849493a6862ef2f
Lucas Silva <lucasfsilva2310@gmail.com>
2023-10-16 20:02:45 -0300

    adding jest and jest-environment-jsdom for initial test structure. Also creating scripts for testing inside package.json

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], // Load setup.ts file before tests
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
+}
